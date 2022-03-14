bf65da61bdc390c15365238bcb18320928cf0f08
StaceyA132 <101208038+StaceyA132@users.noreply.github.com>
2022-03-13 23:51:26 -0400

    Add files via upload

---
+++ PowerOfTwo.java
@@ -0,0 +1,25 @@
+//Write a program that calculates all powers of two from 1 to 10
+// 2 4 8 16 32
+/**
+ * 
+ * Stacey Annan
+ * 
+ * 
+ */ 
+
+public class PowerOfTwo {
+
+	public static void main(String[] args) {
+		int i;
+		for (i = 1; i <= 10; ++i) {
+			
+			System.out.println(Math.pow(2, i));
+		}
+	
+		
+		 
+		
+
+	}
+
+}
